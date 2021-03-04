7a938aac5cc216a87df45a266329182a6b13f509
Fabian Steiner <f@steinernet.at>
2021-03-04 17:23:29 +0100

    ESP serves Web Applikation by itself now

--- WaterstationV2/main/web_server.c
+++ WaterstationV2/main/web_server.c
@@ -4,14 +4,23 @@
 #include <esp_system.h>
 #include <nvs_flash.h>
 #include <sys/param.h>
-#include "nvs_flash.h"
 #include "esp_netif.h"
 #include "esp_eth.h"
 #include <esp_http_server.h>
+#include "esp_vfs.h"
+#include "esp_spiffs.h"
 
 #include "wifi.h"
 #include "variablepool.h"
 #include "jsonParser.h"
+
+#define FILE_PATH_MAX (ESP_VFS_PATH_MAX + 128)
+#define SCRATCH_BUFSIZE 8192
+#define SPIFFS_BASE_PATH  "/webapp"
+
+#define CHECK_FILE_EXTENSION(filename, ext) (strcasecmp(&filename[strlen(filename) - strlen(ext)], ext) == 0)
+
+char chunk[SCRATCH_BUFSIZE] = {0};
 
 
 static const char *TAG = "WebSocket Server";
@@ -155,6 +164,7 @@ static esp_err_t echo_handler(httpd_req_t *req)
     return ret;
 }
 
+
 static const httpd_uri_t ws = {
         .uri        = "/ws",
         .method     = HTTP_GET,
@@ -164,10 +174,90 @@ static const httpd_uri_t ws = {
 };
 
 
+static esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filepath)
+{
+    const char *type = "text/plain";
+    if (CHECK_FILE_EXTENSION(filepath, ".html")) {
+        type = "text/html";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".js")) {
+        type = "application/javascript";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".css")) {
+        type = "text/css";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".png")) {
+        type = "image/png";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".ico")) {
+        type = "image/x-icon";
+    } else if (CHECK_FILE_EXTENSION(filepath, ".svg")) {
+        type = "image/svg+xml";
+    }
+    return httpd_resp_set_type(req, type);
+}
+
+/* Send HTTP response with the contents of the requested file */
+static esp_err_t rest_common_get_handler(httpd_req_t *req)
+{
+    char filepath[FILE_PATH_MAX];
+
+    strlcpy(filepath, SPIFFS_BASE_PATH, sizeof(filepath));
+    if (req->uri[strlen(req->uri) - 1] == '/' || strcmp(req->uri, "/dashboard") == 0) {
+        strlcat(filepath, "/index.html", sizeof(filepath));
+    }else {
+        strlcat(filepath, req->uri, sizeof(filepath));
+    }
+    int fd = open(filepath, O_RDONLY, 0);
+    if (fd == -1) {
+        ESP_LOGE(TAG, "Failed to open file : %s", filepath);
+        /* Respond with 500 Internal Server Error */
+        httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to read existing file");
+        return ESP_FAIL;
+    }
+
+    set_content_type_from_file(req, filepath);
+
+    
+    ssize_t read_bytes;
+    do {
+        /* Read file in chunks into the scratch buffer */
+        read_bytes = read(fd, chunk, SCRATCH_BUFSIZE);
+        if (read_bytes == -1) {
+            ESP_LOGE(TAG, "Failed to read file : %s", filepath);
+        } else if (read_bytes > 0) {
+            /* Send the buffer contents as HTTP response chunk */
+            if (httpd_resp_send_chunk(req, chunk, read_bytes) != ESP_OK) {
+                close(fd);
+                ESP_LOGE(TAG, "File sending failed!");
+                /* Abort sending file */
+                httpd_resp_sendstr_chunk(req, NULL);
+                /* Respond with 500 Internal Server Error */
+                httpd_resp_send_err(req, HTTPD_500_INTERNAL_SERVER_ERROR, "Failed to send file");
+                return ESP_FAIL;
+            }
+        }
+    } while (read_bytes > 0);
+    /* Close file after sending complete */
+    close(fd);
+    ESP_LOGI(TAG, "File sending complete");
+    /* Respond with an empty chunk to signal HTTP response completion */
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
+}
+
+
+ /* URI handler for getting web server files */
+static const httpd_uri_t common_get_uri = {
+    .uri = "/*",
+    .method = HTTP_GET,
+    .handler = rest_common_get_handler,
+    .user_ctx = NULL
+};
+
+
 static httpd_handle_t start_webserver(void)
 {
     httpd_handle_t server = NULL;
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.uri_match_fn = httpd_uri_match_wildcard;
+
 
     // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
@@ -175,6 +265,7 @@ static httpd_handle_t start_webserver(void)
         // Registering the ws handler
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &ws);
+        httpd_register_uri_handler(server, &common_get_uri);
         return server;
     }
 
@@ -210,6 +301,37 @@ static void connect_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
+esp_err_t init_fs(void)
+{
+    esp_vfs_spiffs_conf_t conf = {
+        .base_path = SPIFFS_BASE_PATH,
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = false
+    };
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return ESP_FAIL;
+    }
+
+    size_t total = 0, used = 0;
+    ret = esp_spiffs_info(NULL, &total, &used);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name(ret));
+    } else {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+    return ESP_OK;
+}
+
 void initWebSocketServer(){
     xMutexTriggerAsync = xSemaphoreCreateMutex();
     plantBufferReference = getVariablePool();
@@ -221,6 +343,8 @@ void initWebSocketServer(){
     //ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     initWifi();
+    ESP_ERROR_CHECK(init_fs());
+
 
     /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
      * and re-start it upon connection.
---
+++ WaterstationV2/web-app/precache-manifest.1f452bf699fd240c17bf75adcf71a4e3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b85d458806196996e4373e0fb00892e",
+    "url": "/static/media/paprica.8b85d458.svg"
+  },
+  {
+    "revision": "3aec88828a17625f5158",
+    "url": "/static/css/main.8cb93959.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da18e3c4ca37625f929a",
+    "url": "/static/js/2.46109804.chunk.js"
+  },
+  {
+    "revision": "6904d03d68077fec18362f1f28769220",
+    "url": "/static/media/drops.6904d03d.svg"
+  },
+  {
+    "revision": "b8f4a981f4925562256b2f3d491a97ac",
+    "url": "/static/media/plant.b8f4a981.svg"
+  },
+  {
+    "revision": "123e5a5dbeff88f3d7eb26a4b973898a",
+    "url": "/static/media/tomato.123e5a5d.svg"
+  },
+  {
+    "revision": "3aec88828a17625f5158",
+    "url": "/static/js/main.18620380.chunk.js"
+  },
+  {
+    "revision": "f42a77b104d5b3df3e410904c9365837",
+    "url": "/static/media/chilli.f42a77b1.svg"
+  },
+  {
+    "revision": "954725793d3f9e9e6be2266b4178e4e4",
+    "url": "/static/media/cucumber.95472579.svg"
+  },
+  {
+    "revision": "f30230c43f60178edaf1cfa94017e3e7",
+    "url": "/static/media/microgreens.f30230c4.svg"
+  },
+  {
+    "revision": "2a8bfa959f522a5cc310b32e14d63308",
+    "url": "/static/media/marihuana.2a8bfa95.svg"
+  },
+  {
+    "revision": "00bc6ce87a315d719445b27dd601f3d3",
+    "url": "/static/media/Pot.00bc6ce8.svg"
+  },
+  {
+    "revision": "161d37fe09222f5052adaf770053b13c",
+    "url": "/static/media/plant.161d37fe.jpeg"
+  },
+  {
+    "revision": "f6bfab4634597ceae3459f17808bdf80",
+    "url": "/index.html"
+  }
+];
